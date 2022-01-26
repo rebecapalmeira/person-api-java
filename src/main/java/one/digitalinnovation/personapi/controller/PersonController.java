96abc7543cd3b83f0d51a6e009ee0614ab702d14
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2022-01-26 18:23:55 -0300

    get all built

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -4,11 +4,13 @@ package one.digitalinnovation.personapi.controller;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/person")
@@ -19,6 +21,8 @@ public class PersonController {
 
     private final PersonService personService;
 
+
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
@@ -28,6 +32,12 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+
+        return personService.listAll();
     }
 
 }
