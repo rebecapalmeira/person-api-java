eefe2494ca4347d9263a78ab34021cc7ae6702de
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2022-01-26 11:01:30 -0300

    personDTO and saving person

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,6 +1,12 @@
 package one.digitalinnovation.personapi.controller;
 
 
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/person")
 public class PersonController {
 
-    public String getBook() {
-        return "Testing API!";
+    private final PersonRepository personRepository;
+
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Person created succesfully. Person ID: " + person.getId())
+                .build();
     }
 
 }
